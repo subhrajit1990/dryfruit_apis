2b6c3578c59efc8176979a89e944c4e92c3d7d0e
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2022-04-18 21:03:26 +0530

    Add files via upload
    product files

---
+++ src/main/java/com/manager/domain/Discount.java
@@ -0,0 +1,14 @@
+package com.manager.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "DISCOUNT")
+public class Discount implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/main/java/com/manager/domain/ProductCategory.java
@@ -0,0 +1,14 @@
+package com.manager.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PRODUCT_CATEGORY")
+public class ProductCategory implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}
