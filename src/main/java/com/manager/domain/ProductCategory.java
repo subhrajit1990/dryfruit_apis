0b67ef91419e1365572a2f2d7b0a1e5d4fc77821
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2022-05-08 22:55:54 +0530

    Update ProductCategory.java
    products APIs

--- src/main/java/com/manager/domain/ProductCategory.java
+++ src/main/java/com/manager/domain/ProductCategory.java
@@ -80,5 +80,50 @@ public class ProductCategory implements Serializable {
 		this.products = products;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((desc == null) ? 0 : desc.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((modifiedDt == null) ? 0 : modifiedDt.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((products == null) ? 0 : products.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ProductCategory other = (ProductCategory) obj;
+		if (desc == null) {
+			if (other.desc != null)
+				return false;
+		} else if (!desc.equals(other.desc))
+			return false;
+		if (modifiedDt == null) {
+			if (other.modifiedDt != null)
+				return false;
+		} else if (!modifiedDt.equals(other.modifiedDt))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (products == null) {
+			if (other.products != null)
+				return false;
+		} else if (!products.equals(other.products))
+			return false;
+		return true;
+	}
+
+	
 	
 }
