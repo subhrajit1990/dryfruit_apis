bf60ecdf1859afc6d8acfce235a9c47b4735e33b
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2022-05-08 22:56:24 +0530

    Update ProductInventory.java
    Prpduct APIs

--- src/main/java/com/manager/domain/ProductInventory.java
+++ src/main/java/com/manager/domain/ProductInventory.java
@@ -1,11 +1,16 @@
 package com.manager.domain;
 
 import java.io.Serializable;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -17,7 +22,14 @@ public class ProductInventory implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
+	
+	@Column(name="quantity")
+	private int quantity;
+	
+	@OneToMany(mappedBy = "inventory", fetch = FetchType.LAZY,
+			cascade = CascadeType.ALL)
+	private Set<Product> products;
+	
 	public Long getId() {
 		return id;
 	}
@@ -25,7 +37,36 @@ public class ProductInventory implements Serializable {
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
-	
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + quantity;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ProductInventory other = (ProductInventory) obj;
+		if (quantity != other.quantity)
+			return false;
+		return true;
+	}
 
 }
