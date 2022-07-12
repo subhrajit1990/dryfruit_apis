b4360bab51d4268d64970ed5c7b653b5c6baf00f
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2022-07-12 21:04:54 +0530

    Update IProductService.java

--- src/main/java/com/manager/service/IProductService.java
+++ src/main/java/com/manager/service/IProductService.java
@@ -3,8 +3,11 @@
  */
 package com.manager.service;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Component;
 
+import com.manager.model.AddProductRequest;
 import com.manager.model.ProductRequest;
 import com.manager.model.ProductResponse;
 
@@ -20,5 +23,6 @@ public interface IProductService {
 	public ProductResponse fetchAllProducts(ProductRequest productRequest, String masterTxnRefNo, String channel);
 	public ProductResponse fetchRecentProducts(ProductRequest productRequest, String masterTxnRefNo, String channel);
 	public ProductResponse fetchProduct(String productId, String masterTxnRefNo, String channel);
+	public ProductResponse addProduct(@Valid AddProductRequest addProductRequest, String masterTxnRefNo,String channel);
 
 }
