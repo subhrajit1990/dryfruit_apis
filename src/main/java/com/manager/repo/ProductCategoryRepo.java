ef93fad8524e0a0e0e202544098b61592cfc4fa1
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2022-07-12 21:10:03 +0530

    Add files via upload

---
+++ src/main/java/com/manager/repo/ProductCategoryRepo.java
@@ -0,0 +1,16 @@
+package com.manager.repo;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.manager.domain.ProductCategory;
+
+/**
+ * @author Troublem@ker
+ */
+
+@Repository
+public interface ProductCategoryRepo extends CrudRepository<ProductCategory,Long> {
+
+}
+ 
